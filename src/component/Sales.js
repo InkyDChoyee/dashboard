bed1ab6c5ade13a1e5c07210c332d8c8f7fd6e4d
InkyDChoyee <choyee0421@naver.com>
2024-04-04 16:44:15 +0900

    Feat: set sales data

--- src/component/Sales.js
+++ src/component/Sales.js
@@ -7,7 +7,37 @@ const Sales = () => {
   useEffect(() => {
     fetch("../problem_3_data.json")
       .then((response) => response.json())
-      .then((data) => setSaleData(data))
+      .then((data) => {
+        // 데이터를 조정하여 saleData에 설정
+        const labels = data.sales_rate.map((item) => item.month);
+        const productAData = data.sales_rate.map((item) => item.productA);
+        const productBData = data.sales_rate.map((item) => item.productB);
+        const productCData = data.sales_rate.map((item) => item.productC);
+
+        setSaleData({
+          labels: labels,
+          datasets: [
+            {
+              label: "Product A",
+              data: productAData,
+              borderColor: "rgba(255, 99, 132, 1)",
+              fill: false,
+            },
+            {
+              label: "Product B",
+              data: productBData,
+              borderColor: "rgba(54, 162, 235, 1)",
+              fill: false,
+            },
+            {
+              label: "Product C",
+              data: productCData,
+              borderColor: "rgba(75, 192, 192, 1)",
+              fill: false,
+            },
+          ],
+        });
+      })
       .catch((error) => console.error("데이터 가져오기 실패: ", error));
   }, []);
 
@@ -15,12 +45,20 @@ const Sales = () => {
     return <div>Loading...</div>;
   }
 
-  const { labels, datasets } = saleData;
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
 
   return (
     <div>
       <h1>월별 제품 판매량</h1>
-      <Line data={data}></Line>
+      <Line data={saleData} options={options} />
     </div>
   );
 };
+
+export default Sales;
